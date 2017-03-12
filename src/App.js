f16a139ededbfa18115aa19744a64244aed7ab1d
mcpengelly <pengelly.mat@gmail.com>
2017-03-12 16:09:58 -0400

    title change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 			<div className="App">
 				<div className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
+					<h2>Welcome to React-Todo</h2>
 				</div>
 				<div className="App-intro">
 					<TodoContainer />
